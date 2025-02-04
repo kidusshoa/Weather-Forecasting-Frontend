4f0eefa661bb1cb2979a26882ae8e161893db182
kidusshoa <captainkevin2008@gmail.com>
2025-02-04 21:51:44 +0300

    tsconfig

--- src/components/mapView.tsx
+++ src/components/mapView.tsx
@@ -1,19 +1,6 @@
 "use client";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import L from "leaflet";
-import { Icon } from "leaflet";
-
-// Fix marker icon issue for Leaflet
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png",
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
-  shadowUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
-});
 
 export default function MapView() {
   const position: [number, number] = [9.03, 38.74]; // Addis Ababa
@@ -22,7 +9,7 @@ export default function MapView() {
     <div className="mt-6">
       <h2 className="text-xl font-semibold mb-2">Interactive Weather Map</h2>
       <MapContainer
-        center={position as [number, number]}
+        center={position}
         zoom={6}
         style={{ height: "400px", width: "100%" }}
       >
